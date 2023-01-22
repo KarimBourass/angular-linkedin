e66822228eabb9dc540297960d46eeb3fb5468e8
Karim Bourass <bourasskarim9@gmail.com>
2023-01-22 14:31:12 +0100

    Fix header padding and add notifications badge

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,3 +1,4 @@
+import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './layouts/header/header.component';
@@ -11,7 +12,8 @@ import { ConnectionLevelComponent } from './components/connection-level/connecti
     ConnectionLevelComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   exports:[
     HeaderComponent,
